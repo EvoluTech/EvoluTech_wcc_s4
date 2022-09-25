b50308b893e8f520e1a10fd020bbff04a33097a2
EvoluTech <100075749+EvoluTech@users.noreply.github.com>
2022-09-25 18:15:15 +0300

    Add files via upload

---
+++ js/loading.js
@@ -0,0 +1,7 @@
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    setTimeout(() => {
+        document.querySelector(".backload").style.display="none";
+        document.querySelector("body").style.overflow="initial"
+    },200);
+});
